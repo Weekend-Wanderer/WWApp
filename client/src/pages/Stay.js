5fda8c9bcf3297a6e9e64010aab39c5735f28836
Michael Johnson <michaeljohnson@Michaels-MacBook-Pro.local>
2018-05-22 09:35:13 -0700

    added some stay style

--- client/src/pages/Stay.js
+++ client/src/pages/Stay.js
@@ -9,7 +9,8 @@ class Stay extends Component {
     state = {
         username: "",
         zipcode: "",
-        restaurants: []
+        restaurantsRow1: [],
+        restaurantsRow2: [],
     }
     componentDidMount() {
         const b = this;
@@ -21,17 +22,22 @@ class Stay extends Component {
                 lng: res.data.lng
             });
             axios.post(`/api/yelp/${b.state.zipcode}`).then(function (data) {
-                const restaurants = [];
-                for (let i = 0; i <= 5; i++) {
-                    restaurants.push(data.data[i]);
+                const restaurantsRow1 = [];
+                const restaurantsRow2 = [];
+                for (let i = 0; i <= 2; i++) {
+                    restaurantsRow1.push(data.data[i]);
                 }
+                for (let i = 3; i <= 5; i++) {
+                    restaurantsRow2.push(data.data[i]);
+                }
+
                 b.setState({
-                    restaurants: restaurants
-                    
+                    restaurantsRow1: restaurantsRow1,
+                    restaurantsRow2: restaurantsRow2
+
                 });
-
-
             })
+            
         });
     }
     render() {
@@ -39,13 +45,24 @@ class Stay extends Component {
         return (
             <div>
                 <h1>Hello {this.state.username}, Let's Stay!</h1>
-                <div>{this.state.restaurants.map(restaurant =>
-                    <ImageCard title={restaurant.name} lat={restaurant.coordinates.latitude} lng={restaurant.coordinates.longitude}/>
-                    )
-
-                }
+                <div className="container">
+                    <div className="row">
+                        {this.state.restaurantsRow1.map(restaurant =>
+                            <div className="col-md-4">
+                                <ImageCard title={restaurant.name} lat={restaurant.coordinates.latitude} lng={restaurant.coordinates.longitude} />
+                            </div>
+                        )
+                    }
+                    </div>
+                    <div className="row">
+                        {this.state.restaurantsRow2.map(restaurant =>
+                            <div className="col-md-4">
+                                <ImageCard title={restaurant.name} lat={restaurant.coordinates.latitude} lng={restaurant.coordinates.longitude} />
+                            </div>
+                        )
+                    }
+                    </div>
                 </div>
-
             </div>
         )
     }
