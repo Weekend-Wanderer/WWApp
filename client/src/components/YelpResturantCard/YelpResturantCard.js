a1f3ad8e9d081eb45bd4e953a7257d08618f0172
Michael Johnson <michaeljohnson@Michaels-MacBook-Pro.local>
2018-05-22 11:13:55 -0700

    fixed css for ccards

--- client/src/components/YelpResturantCard/YelpResturantCard.js
+++ client/src/components/YelpResturantCard/YelpResturantCard.js
@@ -4,10 +4,10 @@ import "./style.css";
 const YelpResturantCard = props => {
   return (
     //title={ylp.name} price={ylp.price} rating={ylp.rating} image_url={ylp.image_url
-  <div className="card card-image">
+  <div className="card card-image card-yelp">
     <div className="img-container">
       <img src={props.image_url} className="img-container" />
-      <p className="card-title">{props.title}</p>
+      <p className="card-title card-title-yelp">{props.title}</p>
     </div>
     
   </div>
