4d427a2235fa32552a6038d011c01c9d90f7ae6a
Michael Johnson <michaeljohnson@Michaels-MacBook-Pro.local>
2018-05-22 10:30:59 -0700

    latest commit

--- client/src/pages/Stay.js
+++ client/src/pages/Stay.js
@@ -4,6 +4,7 @@ import axios from "axios";
 import API from '../utils/API';
 import withAuth from '../components/withAuth';
 import ImageCard from "../components/ImageCard";
+import EventCard from "../components/EventCard";
 
 class Stay extends Component {
     state = {
@@ -11,6 +12,8 @@ class Stay extends Component {
         zipcode: "",
         restaurantsRow1: [],
         restaurantsRow2: [],
+        eventsRow1: [],
+        eventsRow2: []
     }
     componentDidMount() {
         const b = this;
@@ -37,6 +40,29 @@ class Stay extends Component {
 
                 });
             })
+            axios.post(`/api/meetup/${b.state.zipcode}`).then(function (data){
+                console.log(data);
+                const eventsRow1 = [];
+                const eventsRow2 = [];
+                for (let i = 0; i <= 2; i++) {
+                    eventsRow1.push({
+                        eventName: data.data[i].name,
+                        eventLink: data.data[i].link
+                    });
+                }
+                for (let i = 3; i <= 5; i++) {
+                    eventsRow2.push({
+                        eventName: data.data[i].name,
+                        eventLink: data.data[i].link
+                    });
+                }
+
+                b.setState({
+                    eventsRow1: eventsRow1,
+                    eventsRow2: eventsRow2
+
+                });
+            })
             
         });
     }
@@ -46,6 +72,7 @@ class Stay extends Component {
             <div>
                 <h1>Hello {this.state.username}, Let's Stay!</h1>
                 <div className="container">
+                    <h2>Places to Eat:</h2>
                     <div className="row">
                         {this.state.restaurantsRow1.map(restaurant =>
                             <div className="col-md-4">
@@ -58,6 +85,23 @@ class Stay extends Component {
                         {this.state.restaurantsRow2.map(restaurant =>
                             <div className="col-md-4">
                                 <ImageCard title={restaurant.name} lat={restaurant.coordinates.latitude} lng={restaurant.coordinates.longitude} />
+                            </div>
+                        )
+                    }
+                    </div>
+                    <h2>Things to Do:</h2>
+                    <div className="row">
+                        {this.state.eventsRow1.map(event =>
+                            <div className="col-md-4">
+                                <EventCard title={event.eventName} link={event.eventLink} />
+                            </div>
+                        )
+                    }
+                    </div>
+                    <div className="row">
+                        {this.state.eventsRow2.map(event =>
+                            <div className="col-md-4">
+                                <EventCard title={event.eventName} link={event.eventLink} />
                             </div>
                         )
                     }
